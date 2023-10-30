db08153e1923ede8cd827941b995107b73d48ba9
1shirisha2 <sirisha.dasari269@gmail.com>
2023-10-30 10:26:21 -0700

    react files

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const addText=()=>{
+    console.log("shirisha")
+  }
+  return (
+    <div className="App">
+      <button onClick={addText}> click</button>
+      </div>)}
+export default App;
